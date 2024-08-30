0bb173ef60c1bc9758a2f6d34432e52f6cbc1399
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-30 16:49:34 +0530

    Handle response and Error

--- FullStackbasic/Megabackend/Back/src/app.js
+++ FullStackbasic/Megabackend/Back/src/app.js
@@ -1,11 +1,30 @@
+// import express from 'express'
+// import cors from 'cors'
+// import cookieParser from 'cookie-parser'
+
+// const app = express();
+
+// app.use(cors({
+//     orgin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+
+// app.use(express.json({limit: "16kb"}))
+// app.use(express.urlencoded({extended: true,limit: "16kb"}))
+// app.use(express.static("public"))
+// app.use(cookieParser())
+
+// export { app } 
+
+
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
-const app = express();
+const app = express()
 
 app.use(cors({
-    orgin: process.env.CORS_ORIGIN,
+    origin: process.env.COR_ORIGIN,
     credentials: true
 }))
 
@@ -14,4 +33,4 @@ app.use(express.urlencoded({extended: true,limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export { app } 
+export { app }
