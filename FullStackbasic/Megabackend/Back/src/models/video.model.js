f24a7889501b954cb3e52dcdef01561862f0d0f7
iSitanshu <Sitanshumishra18@gmail.com>
2024-09-02 13:43:05 +0530

    add user and video model

---
+++ FullStackbasic/Megabackend/Back/src/models/video.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+//allowes to write aggregation queries - first step 
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true
+        },
+        thumbNail: {
+            type: String,
+            required: true
+        },
+        decription: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }, 
+    },{timestamp: true}
+)
+
+//use mongooseAggregationpagination before exxport step two
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video",videoSchema)
